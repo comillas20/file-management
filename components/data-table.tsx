72f65175865294bdbc0aab41ebf19cd3a4fc7949
Jino Joy C. Comillas <jinojoycomillas20@gmail.com>
2024-04-21 20:49:07 +0800

    refactor: Prettier refactors

--- components/data-table.tsx
+++ components/data-table.tsx
@@ -3,71 +3,71 @@
 import { flexRender, Table as T } from "@tanstack/react-table";
 
 import {
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
 } from "@/components/ui/table";
 
 type DataTableProps<TData> = {
-    table: T<TData>;
-    columnLength: number;
+	table: T<TData>;
+	columnLength: number;
 };
 
 export function DataTable<TData>({
-    table,
-    columnLength,
+	table,
+	columnLength,
 }: DataTableProps<TData>) {
-    return (
-        <Table>
-            <TableHeader>
-                {table.getHeaderGroups().map((headerGroup) => (
-                    <TableRow key={headerGroup.id}>
-                        {headerGroup.headers.map((header) => {
-                            return (
-                                <TableHead key={header.id}>
-                                    {header.isPlaceholder
-                                        ? null
-                                        : flexRender(
-                                              header.column.columnDef.header,
-                                              header.getContext()
-                                          )}
-                                </TableHead>
-                            );
-                        })}
-                    </TableRow>
-                ))}
-            </TableHeader>
-            <TableBody>
-                {table.getRowModel().rows?.length ? (
-                    table.getRowModel().rows.map((row) => (
-                        <TableRow
-                            key={row.id}
-                            data-state={row.getIsSelected() && "selected"}
-                            className="data-[state=selected]:bg-primary data-[state=selected]:text-primary-foreground"
-                            onClick={() => row.toggleSelected()}>
-                            {row.getVisibleCells().map((cell) => (
-                                <TableCell key={cell.id}>
-                                    {flexRender(
-                                        cell.column.columnDef.cell,
-                                        cell.getContext()
-                                    )}
-                                </TableCell>
-                            ))}
-                        </TableRow>
-                    ))
-                ) : (
-                    <TableRow>
-                        <TableCell
-                            colSpan={columnLength}
-                            className="h-24 text-center">
-                            No results.
-                        </TableCell>
-                    </TableRow>
-                )}
-            </TableBody>
-        </Table>
-    );
+	return (
+		<Table>
+			<TableHeader>
+				{table.getHeaderGroups().map(headerGroup => (
+					<TableRow key={headerGroup.id}>
+						{headerGroup.headers.map(header => {
+							return (
+								<TableHead key={header.id}>
+									{header.isPlaceholder
+										? null
+										: flexRender(
+												header.column.columnDef.header,
+												header.getContext()
+										  )}
+								</TableHead>
+							);
+						})}
+					</TableRow>
+				))}
+			</TableHeader>
+			<TableBody>
+				{table.getRowModel().rows?.length ? (
+					table.getRowModel().rows.map(row => (
+						<TableRow
+							key={row.id}
+							data-state={row.getIsSelected() && "selected"}
+							className="data-[state=selected]:bg-primary data-[state=selected]:text-primary-foreground"
+							onClick={() => row.toggleSelected()}>
+							{row.getVisibleCells().map(cell => (
+								<TableCell key={cell.id}>
+									{flexRender(
+										cell.column.columnDef.cell,
+										cell.getContext()
+									)}
+								</TableCell>
+							))}
+						</TableRow>
+					))
+				) : (
+					<TableRow>
+						<TableCell
+							colSpan={columnLength}
+							className="h-24 text-center">
+							No data.
+						</TableCell>
+					</TableRow>
+				)}
+			</TableBody>
+		</Table>
+	);
 }
