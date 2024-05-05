7eddd06971e2ccd39d8efffd3d3c5b7e19f0b4bc
Jino Joy C. Comillas <jinojoycomillas20@gmail.com>
2024-05-05 11:49:17 +0800

    chore: Add shadcn/ui command component and change pass validation in login-schema

--- schema/login-schema.ts
+++ schema/login-schema.ts
@@ -2,9 +2,7 @@ import { z } from "zod";
 
 export const loginSchema = z.object({
 	username: z.string().min(8, "Username must be atleast 8 characters"),
-	password: z
-		.string()
-		.min(8, { message: "Password must be 8 characters long" }),
+	password: z.string(),
 });
 
 export type Login = z.infer<typeof loginSchema>;
