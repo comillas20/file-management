d3d024974a82073fb3115d3579a87f33585d52ea
Jino Joy C. Comillas <jinojoycomillas20@gmail.com>
2024-05-22 10:24:13 +0800

    feat: Conditional rendering

--- app/authentication/register/page.tsx
+++ app/authentication/register/page.tsx
@@ -1,9 +1,22 @@
+import { ENABLE_REGISTRATION } from "@/config";
 import { RegisterForm } from "./components/register-form";
+import { Separator } from "@/components/ui/separator";
 
 export default function RegisterPage() {
-	return (
-		<main className="flex items-center justify-center min-h-screen sm:-my-4">
-			<RegisterForm />
-		</main>
-	);
+	if (ENABLE_REGISTRATION)
+		return (
+			<main className="flex items-center justify-center min-h-screen sm:-my-4">
+				<RegisterForm />
+			</main>
+		);
+	else
+		return (
+			<main className="flex items-center justify-center min-h-screen sm:-my-4 bg-muted">
+				<div className="flex gap-4">
+					<h5 className="text-xl font-semibold">404</h5>
+					<Separator orientation="vertical" className="h-auto" />
+					<h5 className="text-lg font-medium">Nuh uh</h5>
+				</div>
+			</main>
+		);
 }
